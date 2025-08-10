52c00ae5ea0f6442970f46613c124e2b50e67c92
KirillTrubitsyn <502198t@gmail.com>
2025-08-10 10:29:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
 self.addEventListener('install', (e)=>{
-  e.waitUntil(
-   caches.open('sommelier-v2').then(cache => cache.addAll([
-'/Sommelier/',
-'/Sommelier/index.html',
-'/Sommelier/manifest.json',
-'/Sommelier/sw.js',
-'/Sommelier/icons/icon-192.png',
-'/Sommelier/icons/icon-512.png',
-'/Sommelier/icons/maskable-512.png'
-]))
-  );
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
-  );
-});
+      e.waitUntil(
+        caches.open('sommelier-v3').then(cache=>cache.addAll([
+          '/index.html','/manifest.json','/sw.js',
+          '/icons/icon-192.png','/icons/icon-512.png','/icons/maskable-512.png'
+        ]))
+      );
+    });
+    self.addEventListener('fetch', (e)=>{
+      e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+    });
