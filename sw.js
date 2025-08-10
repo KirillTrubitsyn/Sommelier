6fdb6d04c2f1ef65b485a347bab209cad41e4af4
KirillTrubitsyn <502198t@gmail.com>
2025-08-10 10:38:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,34 @@
+const CACHE = 'sommelier-v3-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './404.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
 self.addEventListener('install', (e)=>{
-      e.waitUntil(
-        caches.open('sommelier-v3').then(cache=>cache.addAll([
-          '/index.html','/manifest.json','/sw.js',
-          '/icons/icon-192.png','/icons/icon-512.png','/icons/maskable-512.png'
-        ]))
-      );
-    });
-    self.addEventListener('fetch', (e)=>{
-      e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-    });
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event)=>{
+  const req = event.request;
+  // For navigations (opening the app directly), serve index.html to avoid 404 under subpaths
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(r=> r || fetch(req))
+  );
+});
